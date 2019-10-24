c4a4fdfb5f4d4be482c4f042951352f0a5a82571
Sukh Raj Limbu <limburaj65@gmail.com>
2019-10-24 23:22:23 +0530

    Updates

---
+++ precache-manifest.33441841744e0ccb87c29089e360ca65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf9be4b0485b5422490e0370b556d46",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "db7df30114cd022dd157",
+    "url": "/personal/static/js/2.2f3eec24.chunk.js"
+  },
+  {
+    "revision": "3ae89ed09c74b4da9fb6",
+    "url": "/personal/static/js/main.23b0d525.chunk.js"
+  },
+  {
+    "revision": "a77ed42425527fa39246",
+    "url": "/personal/static/js/runtime-main.362444f1.js"
+  },
+  {
+    "revision": "59120f30e95df1d91b85e34071892b27",
+    "url": "/personal/static/media/resume.59120f30.pdf"
+  }
+]);
